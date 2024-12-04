d7d49ddba6f7d9b0e1da0c115329bce383f47a3a
JasperDeSutter <jasper.desutter@gmail.com>
2024-12-04 09:37:50 +0100

    day 04 part 2

--- src/day04.zig
+++ src/day04.zig
@@ -4,19 +4,8 @@ const runner = @import("runner.zig");
 pub const main = runner.run("04", solve);
 
 fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
-    const cols = std.mem.indexOfScalar(u8, input, '\n').? + 1;
-
     _ = alloc;
-    // var debug = try alloc.alloc(u8, input.len);
-    // defer alloc.free(debug);
-    // {
-    //     @memset(debug, '.');
-    //     var i: usize = 0;
-    //     while (i < debug.len - cols + 1) {
-    //         i += cols;
-    //         debug[i - 1] = '\n';
-    //     }
-    // }
+    const cols = std.mem.indexOfScalar(u8, input, '\n').? + 1;
 
     const directions: [8]usize = .{
         1,
@@ -30,23 +19,42 @@ fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
     };
 
     const search = "XMAS";
-    var found: usize = 0;
+    var foundXMAS: usize = 0;
 
     for (0..input.len) |i| {
+        if (input[i] != search[0]) continue;
         for (directions) |d| {
             var j = i;
             for (search) |s| {
                 if (j >= input.len or input[j] != s) break;
                 j +%= d;
             } else {
-                // debug[j -% d] = 'S';
-                found += 1;
+                foundXMAS += 1;
             }
         }
     }
-    // std.debug.print("\n{s}\n\n", .{debug});
 
-    return .{ found, 0 };
+    var foundX_MAS: usize = 0;
+
+    const match = 'M' | 'S';
+    comptime {
+        std.debug.assert('M' | 'A' != match);
+        std.debug.assert('S' | 'A' != match);
+        std.debug.assert('M' | 'X' != match);
+        std.debug.assert('S' | 'X' != match);
+    }
+
+    for ((cols + 1)..(input.len - cols - 1)) |i| {
+        if (input[i] != 'A') continue;
+
+        const pair1 = input[i - cols - 1] | input[i + cols + 1];
+        const pair2 = input[i - cols + 1] | input[i + cols - 1];
+        if (pair1 == match and pair2 == match) {
+            foundX_MAS += 1;
+        }
+    }
+
+    return .{ foundXMAS, foundX_MAS };
 }
 
 test {
@@ -66,6 +74,6 @@ test {
     const example_result: usize = 18;
     const result = try solve(std.testing.allocator, input);
     try std.testing.expectEqual(example_result, result[0]);
-    const example_result2: usize = 0;
+    const example_result2: usize = 9;
     try std.testing.expectEqual(example_result2, result[1]);
 }
