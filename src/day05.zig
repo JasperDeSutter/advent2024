4d9963f084a5f289bb9fbb9d616bfb6754e3b485
JasperDeSutter <jasper.desutter@gmail.com>
2024-12-05 18:20:27 +0100

    day 05 optimize

--- src/day05.zig
+++ src/day05.zig
@@ -4,22 +4,44 @@ const runner = @import("runner.zig");
 pub const main = runner.run("05", solve);
 
 fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
-    _ = alloc;
-
     var middleSum: usize = 0;
     var correctedMiddleSum: usize = 0;
 
+    var rules = try std.ArrayListUnmanaged([4]u8).initCapacity(alloc, 512);
+    defer rules.deinit(alloc);
+    {
+        var i: usize = 0;
+        while (i < input.len) : (i += 6) {
+            if (input[i] == '\n') break;
+            const r = try rules.addOne(alloc);
+            r.* = .{
+                input[i],
+                input[i + 1],
+                input[i + 3],
+                input[i + 4],
+            };
+        }
+    }
+
+    std.mem.sortUnstable([4]u8, rules.items, {}, struct {
+        fn lessThanFn(_: void, lhs: [4]u8, rhs: [4]u8) bool {
+            if (lhs[0] != rhs[0]) return lhs[0] < rhs[0];
+            if (lhs[1] != rhs[1]) return lhs[1] < rhs[1];
+            if (lhs[2] != rhs[2]) return lhs[2] < rhs[2];
+            return lhs[3] < rhs[3];
+        }
+    }.lessThanFn);
+
     const updatesI = std.mem.indexOf(u8, input, "\n\n").?;
-    const rules = input[0..updatesI];
 
     var lines = std.mem.splitScalar(u8, input[updatesI + 2 ..], '\n');
     while (lines.next()) |line| {
         // trim the uninteresting numbers, only need up to one after middle if successful
         const trimmed = line[0 .. (((line.len + 1) / 6) + 2) * 3 - 1];
-        var failedIdx = getFailIdx(trimmed, rules);
+        var failedIdx = getFailIdx(trimmed, rules.items);
 
-        if (failedIdx == trimmed.len) {
-            middleSum += ((trimmed[trimmed.len - 5] - '0') * 10) + trimmed[trimmed.len - 4] - '0';
+        if (failedIdx == 0) {
+            middleSum += ((trimmed[trimmed.len - 5] & 0xf) * 10) + (trimmed[trimmed.len - 4] & 0xf);
             continue;
         }
 
@@ -28,44 +50,41 @@ fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
         @memcpy(lineSlice, line);
         failedIdx = 3;
 
-        while (failedIdx != lineSlice.len) {
+        while (failedIdx != 0) {
             std.mem.swap(u8, &lineSlice[failedIdx - 3 + 0], &lineSlice[failedIdx - 3 + 3]);
             std.mem.swap(u8, &lineSlice[failedIdx - 3 + 1], &lineSlice[failedIdx - 3 + 4]);
 
-            failedIdx = getFailIdx(lineSlice, rules);
+            failedIdx = getFailIdx(lineSlice, rules.items);
         }
 
         const middle = (line.len) / 2;
 
         const idx = lineSlice.len - middle;
-        correctedMiddleSum += ((lineSlice[idx - 1] - '0') * 10) + lineSlice[idx] - '0';
+        correctedMiddleSum += ((lineSlice[idx - 1] & 0xf) * 10) + (lineSlice[idx] & 0xf);
     }
 
     return .{ middleSum, correctedMiddleSum };
 }
 
-fn getFailIdx(line: []const u8, rules: []const u8) usize {
+fn getFailIdx(line: []const u8, rules: []const [4]u8) usize {
     var i: usize = 3;
-    var last: [2]u8 = .{ line[0], line[1] };
 
     while (i < line.len) : (i += 3) {
-        const now: [2]u8 = .{ line[i], line[i + 1] };
-
-        const good: [5]u8 = .{ last[0], last[1], '|', now[0], now[1] };
-        const bad: [5]u8 = .{ now[0], now[1], '|', last[0], last[1] };
-
-        var j: usize = 0;
-        while (j < rules.len) : (j += 6) {
-            const rule = rules[j .. j + 5];
-
-            if (std.mem.eql(u8, &good, rule)) break;
-            if (std.mem.eql(u8, &bad, rule)) return i;
+        if (std.sort.binarySearch([4]u8, line[i - 3 ..][0..5], rules, {}, compareFn) == null) {
+            return i;
         }
-
-        last = now;
     } else {
-        return line.len;
+        return 0;
     }
+}
+
+fn compareFn(_: void, key: *const [5]u8, mid_item: [4]u8) std.math.Order {
+    // key contains the comma, items do not
+    if (key[0] != mid_item[0]) return std.math.order(key[0], mid_item[0]);
+    if (key[1] != mid_item[1]) return std.math.order(key[1], mid_item[1]);
+    if (key[3] != mid_item[2]) return std.math.order(key[3], mid_item[2]);
+    if (key[4] != mid_item[3]) return std.math.order(key[4], mid_item[3]);
+    return std.math.Order.eq;
 }
 
 test {
