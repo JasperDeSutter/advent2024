3404806d6cf5b05d3c8487c0276720cab9d529e9
JasperDeSutter <jasper.desutter@gmail.com>
2024-12-09 10:49:03 +0100

    day 08 part 2

--- src/day08.zig
+++ src/day08.zig
@@ -34,6 +34,9 @@ fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
     var antinodes = try std.DynamicBitSetUnmanaged.initEmpty(alloc, input.len);
     defer antinodes.deinit(alloc);
 
+    var antinodes2 = try std.DynamicBitSetUnmanaged.initEmpty(alloc, input.len);
+    defer antinodes2.deinit(alloc);
+
     {
         var values = antennas.valueIterator();
         while (values.next()) |al| {
@@ -42,24 +45,41 @@ fn solve(alloc: std.mem.Allocator, input: []const u8) anyerror![2]usize {
                 const x1 = pos1 % cols;
                 for (list[i + 1 ..]) |pos2| {
                     const x2 = pos2 % cols;
+                    const delta = pos2 - pos1;
+                    const dx = x2 -% x1;
 
-                    const antiA = pos1 -% (pos2 - pos1);
-                    const xA = x1 -% (x2 -% x1);
+                    const antiA = pos1 -% delta;
+                    const xA = x1 -% dx;
                     if (antiA < input.len and xA < cols - 1) {
                         antinodes.set(antiA);
                     }
 
-                    const antiB = pos2 + (pos2 - pos1);
-                    const xB = x2 +% (x2 -% x1);
+                    const antiB = pos2 + delta;
+                    const xB = x2 +% dx;
                     if (antiB < input.len and xB < cols - 1) {
                         antinodes.set(antiB);
+                    }
+
+                    var j = pos1;
+                    var k = x1;
+                    while (j < input.len) : (j -%= delta) {
+                        // iterate to top left
+                        k -%= dx;
+                    }
+                    j +%= delta;
+
+                    while (j < input.len) : (j += delta) {
+                        k +%= dx;
+                        if (k < cols - 1) {
+                            antinodes2.set(j);
+                        }
                     }
                 }
             }
         }
     }
 
-    return .{ antinodes.count(), 0 };
+    return .{ antinodes.count(), antinodes2.count() };
 }
 
 test {
@@ -81,6 +101,6 @@ test {
     const example_result: usize = 14;
     const result = try solve(std.testing.allocator, input);
     try std.testing.expectEqual(example_result, result[0]);
-    const example_result2: usize = 0;
+    const example_result2: usize = 34;
     try std.testing.expectEqual(example_result2, result[1]);
 }
